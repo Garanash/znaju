6edff734e344831dcf9b7ebc509705564dafa8ee
Зольников Юрий <gakusei96@mail.ru>
2025-02-14 12:42:42 +0300

    Adding connection to users db

--- src/core/config.py
+++ src/core/config.py
@@ -4,12 +4,13 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_title: str = 'App title'
     app_version: str = '1.0.0'
-    database_url: str = 'postgresql+asyncpg://postgres:postgres@db:5432/fastapi'
+    database_url: str = 'postgresql+asyncpg://postgres:postgres@db:5432/fastapi' # noqa
     postgres_db: str = 'fastapi'
     postgres_user: str = 'postgres'
     postgres_password: str = 'postgres'
     db_host: str = 'db'
     db_port: int = 5432
+    secret = 'secret'
 
     class Config:
         env_file = '.env'
